c07411e32db23cc5aebac5f8df44afd79a93db37
Krasimir Karamazov <krasimir.karamazov.ext@racingpost.com>
2019-07-26 16:13:01 +0300

    Sorts

---
+++ src/Sorts.java
@@ -0,0 +1,150 @@
+public class Sorts {
+
+    //SELECTION SORT
+    void sort(int arr[])
+    {
+        int n = arr.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+
+    
+    //INSERTION SORT
+
+    void insertionsort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    //MERGE SORT
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(a, l, r, mid, n - mid);
+    }
+
+    public static void merge(
+            int[] a, int[] l, int[] r, int left, int right) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            }
+            else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+
+    //QUICK SORT
+
+    int partition(int arr[], int left, int right)
+
+    {
+
+        int i = left, j = right;
+
+        int tmp;
+
+        int pivot = arr[(left + right) / 2];
+
+
+
+        while (i <= j) {
+
+            while (arr[i] < pivot)
+
+                i++;
+
+            while (arr[j] > pivot)
+
+                j--;
+
+            if (i <= j) {
+
+                tmp = arr[i];
+
+                arr[i] = arr[j];
+
+                arr[j] = tmp;
+
+                i++;
+
+                j--;
+
+            }
+
+        };
+
+
+
+        return i;
+
+    }
+
+
+
+    void quickSort(int arr[], int left, int right) {
+
+        int index = partition(arr, left, right);
+
+        if (left < index - 1)
+
+            quickSort(arr, left, index - 1);
+
+        if (index < right)
+
+            quickSort(arr, index, right);
+
+    }
+
+}
