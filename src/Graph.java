62587d30a0ca37bcd770dfd18154881d0705c624
Krasimir Karamazov <krasimir.karamazov.ext@racingpost.com>
2019-07-26 17:14:09 +0300

    Sorts

--- src/Graph.java
+++ src/Graph.java
@@ -17,6 +17,57 @@ public class Graph {
         graphData.putIfAbsent(new Vertex(value), new ArrayList<>());
     }
 
+    void addEdge(String label1, String label2) {
+        Vertex v1 = new Vertex(label1);
+        Vertex v2 = new Vertex(label2);
+        graphData.get(v1).add(v2);
+        graphData.get(v2).add(v1);
+    }
+
+    void removeEdge(String label1, String label2) {
+        Vertex v1 = new Vertex(label1);
+        Vertex v2 = new Vertex(label2);
+        List<Vertex> eV1 = graphData.get(v1);
+        List<Vertex> eV2 = graphData.get(v2);
+        if (eV1 != null)
+            eV1.remove(v2);
+        if (eV2 != null)
+            eV2.remove(v1);
+    }
+
+    Set<String> depthFirstTraversal(Graph graph, String root) {
+        Set<String> visited = new LinkedHashSet<String>();
+        Stack<String> stack = new Stack<String>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            String vertex = stack.pop();
+            if (!visited.contains(vertex)) {
+                visited.add(vertex);
+                for (Vertex v : graph.getAdjVertices(vertex)) {
+                    stack.push(v.label);
+                }
+            }
+        }
+        return visited;
+    }
+
+    Set<String> breadthFirstTraversal(Graph graph, String root) {
+        Set<String> visited = new LinkedHashSet<String>();
+        Queue<String> queue = new LinkedList<String>();
+        queue.add(root);
+        visited.add(root);
+        while (!queue.isEmpty()) {
+            String vertex = queue.poll();
+            for (Vertex v : graph.getAdjVertices(vertex)) {
+                if (!visited.contains(v.label)) {
+                    visited.add(v.label);
+                    queue.add(v.label);
+                }
+            }
+        }
+        return visited;
+    }
+
     void BFS(int s)
     {
         // Mark all the vertices as not visited(By default
